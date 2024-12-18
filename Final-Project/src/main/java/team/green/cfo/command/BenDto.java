c0f8337efe6ca9ccd413b59208b4592a3ad136b1
V <manuelvieira1379@gmail.com>
2024-12-18 18:31:40 +0000

    changes

--- Final-Project/src/main/java/team/green/cfo/command/BenDto.java
+++ Final-Project/src/main/java/team/green/cfo/command/BenDto.java
@@ -1,28 +1,29 @@
 package team.green.cfo.command;
 
+import javax.validation.constraints.*;
+
 public class BenDto {
 
-//<<<<<<< HEAD
     private Integer id;
 
-    /*@NotNull*/ (message = "First name is mandatory")
-    /*@NotBlan*/ (message = "First name is mandatory")
-    /*@Size*/ (min = 3, max = 64)
+    @NotNull(message = "First name is mandatory")
+    @NotBlank (message = "First name is mandatory")
+    @Size(min = 3, max = 64)
     private String firstName;
 
-    /*@NotNull*/ (message = "First name is mandatory")
-    /*@NotBlank*/ (message = "First name is mandatory")
-    /*@Size*/ (min = 3, max = 64)
+    @NotNull (message = "First name is mandatory")
+    @NotBlank (message = "First name is mandatory")
+    @Size (min = 3, max = 64)
     private String lastName;
 
 
     @Email
-    @NotBlank (message = "Email is mandatory")
+    @NotBlank(message = "Email is mandatory")
     private String email;
 
      
-    /*@Pattern*/ (regexp = "^\\+?[0-9]*$", message = "Phone number contains invalid characters")
-    /*@Size*/ (min = 9, max = 16)
+    @Pattern(regexp = "^\\+?[0-9]*$", message = "Phone number contains invalid characters")
+    @Size (min = 9, max = 16)
     private String phone;
 /*
 =======
