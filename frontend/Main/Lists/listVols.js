373447431ef90088db7fa430b6cb350201548deb
naturesa <ana.teresa.f@gmail.com>
2024-12-18 19:49:30 +0000

    cards transparentes

--- frontend/Main/Lists/listVols.js
+++ frontend/Main/Lists/listVols.js
@@ -1,32 +1,31 @@
-const API_URL = "http://localhost:8080/helphub";
-
-const createButton = () => {
-	const buttonContainer = document.createElement('div');
-	buttonContainer.className = 'button-container';
-
-	const button = document.createElement('button');
-	button.innerText = 'Play Round';
-
-	button.addEventListener('click', playRound);
-	buttonContainer.appendChild(button);
-	return buttonContainer;
-}
-
 window.onload = function () {
-
-	const container = document.querySelector('.container-app');
-
-	const game = document.createElement('div');
-	game.className = 'game';
-
-	const p1Div = createPlayerContainer(1);
-	const p2Div = createPlayerContainer(2);
-
-	game.appendChild(p1Div);
-	game.appendChild(p2Div);
-
-	const buttonContainer = createButton();
-
-	container.appendChild(game);
-	container.appendChild(buttonContainer);
-}
+	const swiper = new Swiper('.slider-wrapper', {
+		loop: true,
+		grabCursor: true,
+		spaceBetween: 30,
+		// Pagination bullets
+		pagination: {
+		  el: '.swiper-pagination',
+		  clickable: true,
+		  dynamicBullets: true
+		},
+		// Navigation arrows
+		navigation: {
+		  nextEl: '.swiper-button-next',
+		  prevEl: '.swiper-button-prev',
+		},
+		// Responsive breakpoints
+		breakpoints: {
+		  0: {
+			slidesPerView: 1
+		  },
+		  768: {
+			slidesPerView: 2
+		  },
+		  1024: {
+			slidesPerView: 3
+		  }
+		}
+	  });
+  }
+  
