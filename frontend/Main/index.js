e2d160081fd38953990537a3b2414399e56668d9
naturesa <ana.teresa.f@gmail.com>
2024-12-18 14:25:37 +0000

    few changes

--- frontend/Main/index.js
+++ frontend/Main/index.js
@@ -1,3 +1,5 @@
+const API_URL = "http://localhost:8080/helphub";
+
 const createButton = () => {
 	const buttonContainer = document.createElement('div');
 	buttonContainer.className = 'button-container';
@@ -8,4 +10,23 @@ const createButton = () => {
 	button.addEventListener('click', playRound);
 	buttonContainer.appendChild(button);
 	return buttonContainer;
+}
+
+window.onload = function () {
+
+	const container = document.querySelector('.container-app');
+
+	const game = document.createElement('div');
+	game.className = 'game';
+
+	const p1Div = createPlayerContainer(1);
+	const p2Div = createPlayerContainer(2);
+
+	game.appendChild(p1Div);
+	game.appendChild(p2Div);
+
+	const buttonContainer = createButton();
+
+	container.appendChild(game);
+	container.appendChild(buttonContainer);
 }
