d76decbf6edce2a59b19636a4ed0a17654734cec
Lucas Takase <lucastakase@gmail.com>
2024-12-18 16:58:23 +0000

    sdfg

--- Final-Project/src/main/java/team/green/cfo/daos/jpa/JpaBenDao.java
+++ Final-Project/src/main/java/team/green/cfo/daos/jpa/JpaBenDao.java
@@ -1,8 +1,9 @@
 package team.green.cfo.daos.jpa;
 
+import org.springframework.stereotype.Repository;
 import team.green.cfo.daos.BenDao;
 import team.green.cfo.models.BenModel;
-
+@Repository
 public class JpaBenDao extends GenericJpaDao<BenModel> implements BenDao {
 
 
